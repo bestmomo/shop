1054b2c30d44cdfa146e72e1cee0f12503477604
GrCOTE7 <grcote7@gmail.com>
2025-01-20 18:28:29 +0100

    build: update vite config refresh option
    - Hot refresh for all php scripts under app/
    - Good VAT calculation

--- app/helpers.php
+++ app/helpers.php
@@ -2,8 +2,8 @@
 
 if (!function_exists('price_without_vat')) {
 
-    function price_without_vat(float $price_with_vat, int $vat_rate = 20): float 
+    function price_without_vat(float $price_with_vat, int $vat_rate = 20): float
     {
-        return round($price_with_vat / (1.0 + (float)env('VAT_RATE', $vat_rate)), 2);
+        return round($price_with_vat / (1 + (float)env('VAT_RATE', $vat_rate) / 100), 2);
     }
 }

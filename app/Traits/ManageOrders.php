c2b29c3ed904eae34552b2e6032e98418030d4aa
GrCOTE7 <grcote7@gmail.com>
2025-02-04 18:50:25 +0100

    Fix: Acc customer sorting in lists

--- app/Traits/ManageOrders.php
+++ app/Traits/ManageOrders.php
@@ -2,19 +2,19 @@
 
 namespace App\Traits;
 
-trait ManageOrders 
+trait ManageOrders
 {
     public array $sortBy = [
         'column' => 'created_at',
         'direction' => 'desc',
     ];
-    
+
     public function headersOrders(): array
     {
         return [
             ['key' => 'id', 'label' => __('Id')],
-            ['key' => 'reference', 'label' => __('Reference')], 
-            ['key' => 'user', 'label' => __('Customer'), 'sortable' => false],            
+            ['key' => 'reference', 'label' => __('Reference')],
+            ['key' => 'user', 'label' => __('Customer')],
             ['key' => 'total', 'label' => __('Total price')],
             ['key' => 'created_at', 'label' => __('Date')],
             ['key' => 'state', 'label' => __('Etat'), 'sortable' => false],

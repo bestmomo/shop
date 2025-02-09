0b30e69be9356b5279fc9e4ffbfef05555478a0d
GrCOTE7 <grcote7@gmail.com>
2025-02-09 19:38:59 +0100

    Fix: Avoid duplicate code by using sill's component for address instead of another partial (used on popover order listing)

--- resources/views/components/address.blade.php
+++ resources/views/components/address.blade.php
@@ -1,18 +1,27 @@
 <ul class="p-2 mb-2">
-    @isset($address->name)
-        <li class="font-bold">{{ "$address->civility. $address->name $address->firstname" }}</li>
-    @endif
+
     @if ($address->company)
         <li class="font-bold">{{ $address->company }}</li>
     @endif
+
+    @isset($address->name)
+        <li>{{ "$address->civility. $address->name $address->firstname" }}</li>
+    @endif
+
+
         <li>{{ $address->address }}</li>
+
     @if ($address->addressbis)
         <li>{{ $address->addressbis }}</li>
     @endif
+
     @if ($address->bp)
         <li>{{ $address->bp }}</li>
     @endif
-    <li>{{ "$address->postal $address->city" }}</li>
-    <li class="font-bold">{{ $address->country->name }}</li>
-    <li><x-icon name="o-phone" /><em>{{ $address->phone }}</em></li>
+
+    <li>{{ $address->postal .' '. strtoupper($address->city) }}</li>
+<hr>
+    <li class="font-bold uppercase text-right">{{ $address->country->name }}</li>
+
+    <li><x-icon name="o-phone"  class="text-green-400"/><em>{{ $address->phone }}</em></li>
 </ul>
